e36d298f5a8de9b32baac298d5d7795404e6a97f
Salina Tulik <salina.tulik@gmail.com>
2024-11-04 20:50:57 -0700

    add session provider to authprovider

--- src/components/providers/AuthProvider.tsx
+++ src/components/providers/AuthProvider.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-type AuthProviderProps = {
-  children: React.ReactNode;
-};
+import { SessionProvider } from "next-auth/react";
 
-export function AuthProvider({ children }: AuthProviderProps) {
-  return <>{children}</>;
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
+
+// "use client";
+
+// type AuthProviderProps = {
+//   children: React.ReactNode;
+// };
+
+// export function AuthProvider({ children }: AuthProviderProps) {
+//   return <>{children}</>;
+// }
